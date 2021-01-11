2d532034964280404861a7d48336b8c5f17ef0fd
Rafata8 <rafatimer@hotmail.com>
2021-01-11 18:22:03 +0100

    all finished

--- src/main/java/es/upm/master/exercise3.java
+++ src/main/java/es/upm/master/exercise3.java
@@ -81,24 +81,29 @@ public class exercise3 {
 
                 ).keyBy(1);
 
-        KeyedStream<Tuple5<Integer, Integer, Integer, Integer, Integer>, Tuple> keyedStream2 = mapStream.
-                assignTimestampsAndWatermarks(
-                        new AscendingTimestampExtractor<Tuple5<Integer, Integer, Integer, Integer, Integer>>() {
-                            @Override
-                            public long extractAscendingTimestamp(Tuple5<Integer, Integer, Integer, Integer, Integer> element) {
-                                return element.f0*1000; ///ahi habia un *1000
-                            }
-                        }
-
-                ).keyBy(3);
 
 
-        SingleOutputStreamOperator<Tuple3<Integer, Integer, Integer>> sumTumblingEventTimeWindows1 =
+        //output2, agrupamos porxway y calculamos la solucion 1, pero con timestamp (luego lo eliminaremos)
+        //avgspeed2 calcula las velocidades medias
+        SingleOutputStreamOperator<Tuple4<Integer, Integer, Integer,Integer>> sumTumblingEventTimeWindows =
                 keyedStream.window(TumblingEventTimeWindows.of(Time.seconds((long) 3600))).apply(new AvgSpeed());
 
-        SingleOutputStreamOperator<Tuple3<Integer, Integer, Integer>> sumTumblingEventTimeWindows2 =
-                keyedStream2.window(TumblingEventTimeWindows.of(Time.seconds((long) 3600))).apply(new MaxSpeed());
 
+        KeyedStream<Tuple4<Integer, Integer, Integer,Integer>, Tuple> keyedStreamEx2=sumTumblingEventTimeWindows.keyBy(1);
+
+        //a la solucioon 2 le pasamos la solucoion 1(tiene timestamp, podemos filtrar)
+        //maxspeed2 coge la maxima velocidad
+        SingleOutputStreamOperator<Tuple3<Integer, Integer, Integer>> sol2 =
+                keyedStreamEx2.window(TumblingEventTimeWindows.of(Time.seconds((long) 3600))).apply(new MaxSpeed());
+
+        //eliminamos el ts de la sol1
+        SingleOutputStreamOperator<Tuple3<Integer, Integer, Integer>> sol1 =sumTumblingEventTimeWindows.map(new MapFunction<Tuple4<Integer, Integer, Integer,Integer>, Tuple3<Integer, Integer, Integer>>() {
+                    public Tuple3<Integer, Integer, Integer> map(Tuple4<Integer, Integer, Integer,Integer> in) throws Exception{
+                        Tuple3<Integer, Integer, Integer> out = new Tuple3(in.f0,
+                               in.f1,in.f2);
+                        return out;
+                    }
+                });
 
 
 
@@ -106,19 +111,52 @@ public class exercise3 {
         // emit result
         if (params.has("output1")) {
             //sumTumblingEventTimeWindows1.writeAsText(params.get("output1"));
-            sumTumblingEventTimeWindows1.writeAsCsv(params.get("output1"));
+            sol1.writeAsCsv(params.get("output1"));
         }
 
         if (params.has("output2")) {
-            sumTumblingEventTimeWindows2.writeAsCsv(params.get("output2"));
+            sol2.writeAsCsv(params.get("output2"));
         }
 
         // execute program
         env.execute("exercise3");
     }
 
-    public static class AvgSpeed implements WindowFunction<Tuple5<Integer, Integer, Integer, Integer, Integer>, Tuple3<Integer, Integer, Integer>, Tuple, TimeWindow> {
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple5<Integer, Integer, Integer, Integer, Integer>> input, Collector<Tuple3<Integer, Integer, Integer>> out) throws Exception {
+
+
+    public static class MaxSpeed implements WindowFunction<Tuple4<Integer, Integer, Integer,Integer>, Tuple3<Integer, Integer, Integer>, Tuple, TimeWindow> {
+        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple4<Integer, Integer, Integer,Integer>> input, Collector<Tuple3<Integer, Integer, Integer>> out) throws Exception {
+            Iterator<Tuple4<Integer, Integer, Integer,Integer>> iterator = input.iterator();
+            Tuple4<Integer, Integer, Integer,Integer> first = iterator.next();
+
+            Integer vid=0;
+            Integer avgSpeed = 0;
+            Integer xway=0;
+            //Integer quantity=0;
+
+
+            if (first != null) {
+                vid = first.f0;
+
+                avgSpeed=first.f2;
+                xway=first.f1;
+                //  quantity=1;
+            }
+            while (iterator.hasNext()) {
+                Tuple4<Integer, Integer, Integer,Integer> next = iterator.next();
+                if(next.f2>avgSpeed) {
+                    avgSpeed = next.f2;
+                    vid=next.f0;
+                }
+                //quantity += 1;
+            }
+            //avgSpeed=avgSpeed/quantity;
+            out.collect(new Tuple3<Integer, Integer, Integer>(vid, xway, avgSpeed));
+        }
+    }
+
+    public static class AvgSpeed implements WindowFunction<Tuple5<Integer, Integer, Integer, Integer, Integer>, Tuple4<Integer, Integer, Integer,Integer>, Tuple, TimeWindow> {
+        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple5<Integer, Integer, Integer, Integer, Integer>> input, Collector<Tuple4<Integer, Integer, Integer,Integer>> out) throws Exception {
             Iterator<Tuple5<Integer, Integer, Integer, Integer, Integer>> iterator = input.iterator();
             Tuple5<Integer, Integer, Integer, Integer, Integer> first = iterator.next();
             Integer ts = 0;
@@ -130,7 +168,7 @@ public class exercise3 {
 
             if (first != null) {
                 vid = first.f1;
-                //ts = first.f0;
+                ts = first.f0;
                 avgSpeed=first.f2;
                 xway=first.f3;
                 quantity=1;
@@ -141,38 +179,7 @@ public class exercise3 {
                 quantity += 1;
             }
             avgSpeed=avgSpeed/quantity;
-            out.collect(new Tuple3<Integer, Integer, Integer>(vid, xway, avgSpeed));
-        }
-    }
-
-    public static class MaxSpeed implements WindowFunction<Tuple5<Integer, Integer, Integer, Integer, Integer>, Tuple3<Integer, Integer, Integer>, Tuple, TimeWindow> {
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple5<Integer, Integer, Integer, Integer, Integer>> input, Collector<Tuple3<Integer, Integer, Integer>> out) throws Exception {
-            Iterator<Tuple5<Integer, Integer, Integer, Integer, Integer>> iterator = input.iterator();
-            Tuple5<Integer, Integer, Integer, Integer, Integer> first = iterator.next();
-            Integer ts = 0;
-            Integer vid=0;
-            Integer avgSpeed = 0;
-            Integer xway=0;
-            //Integer quantity=0;
-
-
-            if (first != null) {
-                vid = first.f1;
-                ts = first.f0;
-                avgSpeed=first.f2;
-                xway=first.f3;
-              //  quantity=1;
-            }
-            while (iterator.hasNext()) {
-                Tuple5<Integer, Integer, Integer, Integer, Integer> next = iterator.next();
-                if(next.f2>avgSpeed) {
-                    avgSpeed = next.f2;
-                    vid=next.f1;
-                }
-                //quantity += 1;
-            }
-            //avgSpeed=avgSpeed/quantity;
-            out.collect(new Tuple3<Integer, Integer, Integer>(vid, xway, avgSpeed));
+            out.collect(new Tuple4<Integer, Integer, Integer,Integer>(vid, xway, avgSpeed,ts));
         }
     }
 
